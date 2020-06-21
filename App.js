7cbab6c69966a493cb9d52a760eaff1376863980
christianbailon <xtian.bailon@gmail.com>
2020-06-21 09:58:00 +0800

    Setting up the basic structure of the game

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
+/**
+ * Challenge: build the basic structure of our game
+ *
+ * 1. <h1> title at the top
+ * 2. <textarea> for the box to type in
+ *      (tip: React normalizes <textarea /> to be more like <input />,
+ *      so it can be used as a self-closing element and uses the `value` property
+ *      to set its contents)
+ * 3. <h4> ti display the amount of time remaining
+ * 4. <button> to start the game
+ * 5. Another <h1> to display the word count
+ */
+
 import React from 'react';
+import './src/styles/style.scss';
 
 function App() {
   return (
     <div>
-      <h1>Initial React Setup</h1>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Remaining Time:</h4>
+      <button>Start</button>
+      <h1>Word Count:</h1>
     </div>
   );
 }
